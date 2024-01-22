226580402664d38fa259a57cffcc337589ae1cf4
Md Wahiduzzaman Emon <wahedemon09@gmail.com>
2024-01-22 22:49:26 +0600

    new version upgrade

--- src/Utils/getSystemTheme.tsx
+++ src/Utils/getSystemTheme.tsx
@@ -2,7 +2,6 @@
 // const isBrowser = () => typeof window !== 'undefined'; //The approach recommended by Next.js
 
 const getStystemPreference = () => {
-  
   if (typeof window === "undefined") return false;
 
   return window.matchMedia("(prefers-color-scheme: dark)").matches;
